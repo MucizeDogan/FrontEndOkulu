fdae8c00632f743b8b4c006a632c88435e0dd2d2
M-racle <108946729+M-racle@users.noreply.github.com>
2026-05-19 23:30:35 +0300

    fetch txt and json

--- Dersler/Ders005-Js-Http-Async/03-Fetch/01-fetch.js
+++ Dersler/Ders005-Js-Http-Async/03-Fetch/01-fetch.js
@@ -4,12 +4,35 @@ function MyGetTextFile(){
 
     fetch("Data.txt")
         .then((response) => response.text())
-        .then((data) => console.log(data)); //Nu şekilde responsedan gelen verinin yani result u bir daata değişkenine alıp direkt veriyi yazdırmış olduk
+        .then((data) => console.log(data)) //Bu şekilde responsedan gelen verinin yani result u bir data değişkenine alıp direkt veriyi yazdırmış olduk
+        .catch((err) => console.log(err));
+}
 
-
+function MyGetJsonFile(yazdirmaSekli){
+    switch(yazdirmaSekli){
+        case 0:
+        fetch("users.json")
+            .then((response) => response.json()) // Json u alıp parse ediyoruz sonra değişkene atıp yazdırıyoruz. Array olarak dönüyor.
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
+        break;
+        case 1:
+        fetch("users.json")
+            .then((response) => response.json())
+            .then((data) => data.map((user) => console.log(user.name, user.department, user.salary)))
+            .catch((err) => console.log(err))
+        break;
+    }
+    
 }
 
 document
     .getElementById("btnGetText")
     .addEventListener("click", MyGetTextFile);
 //MyGetTextFile();
+
+
+//document.getElementById("btnGetJson").addEventListener("click", MyGetJsonFile(0)); // bu şekilde parametreli de bu şekilde kullanırsan event çalışmıyor
+document
+    .getElementById("btnGetJson")
+    .addEventListener("click", () => MyGetJsonFile(1)); // parametreli yapıda arrow function kullanmalıyım ki çağırınca çalışsın
